285e453d4a495300bd1c79fee32d539d5856304b
Andy Craze <accraze@gmail.com>
2022-08-19 20:33:37 -0700

    interal transform monad

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,13 +1,15 @@
-package example
+package ml_test
 
 trait PipelineOperator [-T, +U] {
   def |> (data: T): Option[U]
 }
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+class _FCT[+T](val _fct: T) {
+  def map[U](c: T => U): _FCT[U] = new _FCT[U]( c(_fct))
+  def flatMap[U](f: T =>_FCT[U]): _FCT[U] = f(_fct)
+  def filter(p: T =>Boolean): _FCT[T] = if( p(_fct) ) new _FCT[T](_fct) else zeroFCT(_fct)
+  def reduceLeft[U](f: (U,T) => U)(implicit c: T=> U): U = f(c(_fct),_fct)
+  def foldLeft[U](zero: U)(f: (U, T) => U)(implicit c: T=> U): U = f(c(_fct), _fct)
+  def foreach(p: T => Unit): Unit = p(_fct)
 }
